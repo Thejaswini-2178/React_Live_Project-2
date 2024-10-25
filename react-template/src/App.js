858d5cf48d197cb7a28065409e2f3732003e8a8c
Thejaswini-2178 <thejaswini2178@gmail.com>
2024-10-25 10:33:16 +0530

    React-Template

---
+++ react-template/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Footer from './React-Template/Footer';
+import Header from './React-Template/Header';
+import Section from './React-Template/Section';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Section/>
+      <Footer/>
+     
+    </div>
+  );
+}
+
+export default App;
